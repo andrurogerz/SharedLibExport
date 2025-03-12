5f5d9b45ed1b56f4a3436c7df0385ec6bd0022be
Andrew Rogers <andrurogerz@gmail.com>
2025-03-12 15:31:12 -0700

    minor cleanup

--- Sources/include/Annotations.h
+++ Sources/include/Annotations.h
@@ -7,17 +7,19 @@
   #else
     #define SDK_ABI __declspec(dllimport)
   #endif
+  // On Windows, the type itself does not need to be exported as long as all
+  // externally referenced members of the type are exported.
   #define SDK_TYPE_ABI
-#elif __has_attribute(visibility)
+#elif defined(__has_attribute) && __has_attribute(visibility)
   #define SDK_ABI __attribute__((visibility("default")))
   #if __has_attribute(__type_visibility__)
     // Use the __type_visibility__ attribute to on classes where type info or
-    // vtable need to be visible externally.
+    // vtable need to be visible externally. This is only supported on clang.
     #define SDK_TYPE_ABI __attribute__((__type_visibility__("default")))
   #else
-    // gcc does not support the __type_visibility__ attribute so resort to using
-    // the visibility attribute. This will export all members of the type, not
-    // just type info and vtable.
+    // The __type_visibility__ attribute is not supported on gcc, so resort to
+    // the visibility attribute. This attribute will export all members of the
+    // type, not just type info and vtable.
     #warning("__type_visibility__ not supported; all members will be exported!")
     #define SDK_TYPE_ABI SDK_ABI
   #endif
